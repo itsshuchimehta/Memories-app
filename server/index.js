66471a078a5bf181e06bae56a9467f8b1d5cbfd8
itsshuchimehta <itsshuchimehta@gmail.com>
2026-05-17 16:16:56 -0400

    Secure database credentials using dotenv

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv";
 
 import postRoutes from "./routes/posts.js";
 
@@ -13,8 +14,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-const CONNECTION_URL =
-  "mongodb+srv://itsshuchimehta:itsshuchimehta123@cluster0.reqwtj1.mongodb.net/?appName=Cluster0";
+const CONNECTION_URL = process.env.CONNECTION_URL;
 
 const PORT = process.env.PORT || 5001;
 

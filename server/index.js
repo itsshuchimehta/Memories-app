6026a9d138109fad718c0591517d62b113f787fa
itsshuchimehta <itsshuchimehta@gmail.com>
2026-05-19 17:53:23 -0400

    Created Posts, post, form component Connected redux to fetch the post

--- server/index.js
+++ server/index.js
@@ -7,12 +7,13 @@ import dotenv from "dotenv";
 import postRoutes from "./routes/posts.js";
 
 const app = express();
-
-app.use("/posts", postRoutes);
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+app.use("/posts", postRoutes);
 
 const CONNECTION_URL = process.env.CONNECTION_URL;
 
